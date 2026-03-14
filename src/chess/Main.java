14ab77e8a5b37ed9cd933b81220b42fd868eabb4
Danillo Augusto <danilloaugusto568@gmail.com>
2026-03-14 17:33:52 -0300

    feat:adicionando a classe Board e imprimir o estado inicial do tabuleiro

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,22 +1,10 @@
 package chess;
 
-import chess.pieces.Pawn;
-import chess.pieces.King;
+import chess.board.Board;
 
 public class Main {
     public static void main(String[] args) {
-        
-        // Criando algumas peças de teste
-        Pawn pawnWhite = new Pawn(Color.WHITE, 6, 0);
-        Pawn pawnBlack = new Pawn(Color.BLACK, 1, 0);
-        King kingWhite = new King(Color.WHITE, 7, 4);
-        King kingBlack = new King(Color.BLACK, 0, 4);
-
-        // Imprimindo os símbolos
-        System.out.println("Peças criadas:");
-        System.out.println("Peão branco: " + pawnWhite.getSymbol());
-        System.out.println("Peão preto: " + pawnBlack.getSymbol());
-        System.out.println("Rei branco: " + kingWhite.getSymbol());
-        System.out.println("Rei preto: " + kingBlack.getSymbol());
+        Board board = new Board();
+        board.print();
     }
 }
---
+++ src/chess/board/Board.java
@@ -0,0 +1,64 @@
+package chess.board;
+
+import chess.Color;
+import chess.pieces.*;
+
+public class Board {
+
+    private Piece[][] squares;
+
+    public Board() {
+        squares = new Piece[8][8];
+        setup();
+    }
+
+    private void setup() {
+        // Peças pretas
+        squares[0][0] = new Rook(Color.BLACK, 0, 0);
+        squares[0][1] = new Knight(Color.BLACK, 0, 1);
+        squares[0][2] = new Bishop(Color.BLACK, 0, 2);
+        squares[0][3] = new Queen(Color.BLACK, 0, 3);
+        squares[0][4] = new King(Color.BLACK, 0, 4);
+        squares[0][5] = new Bishop(Color.BLACK, 0, 5);
+        squares[0][6] = new Knight(Color.BLACK, 0, 6);
+        squares[0][7] = new Rook(Color.BLACK, 0, 7);
+
+        for (int col = 0; col < 8; col++) {
+            squares[1][col] = new Pawn(Color.BLACK, 1, col);
+        }
+
+        // Peças brancas
+        squares[7][0] = new Rook(Color.WHITE, 7, 0);
+        squares[7][1] = new Knight(Color.WHITE, 7, 1);
+        squares[7][2] = new Bishop(Color.WHITE, 7, 2);
+        squares[7][3] = new Queen(Color.WHITE, 7, 3);
+        squares[7][4] = new King(Color.WHITE, 7, 4);
+        squares[7][5] = new Bishop(Color.WHITE, 7, 5);
+        squares[7][6] = new Knight(Color.WHITE, 7, 6);
+        squares[7][7] = new Rook(Color.WHITE, 7, 7);
+
+        for (int col = 0; col < 8; col++) {
+            squares[6][col] = new Pawn(Color.WHITE, 6, col);
+        }
+    }
+
+    public Piece getPiece(int row, int col) {
+        return squares[row][col];
+    }
+
+    public void print() {
+        System.out.println("  a  b  c  d  e  f  g  h");
+        for (int row = 0; row < 8; row++) {
+            System.out.print((8 - row) + " ");
+            for (int col = 0; col < 8; col++) {
+                if (squares[row][col] == null) {
+                    System.out.print(".  ");
+                } else {
+                    System.out.print(squares[row][col].getSymbol() + "  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("  a  b  c  d  e  f  g  h");
+    }
+}
